33c4ffa3df2a4850fa89c7eeea18147c19a08a3b
Joseph McCombs <josephmccombs@Josephs-MacBook-Pro.local>
2018-11-21 15:19:15 -0500

    User Intercace First Commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+//init win
+let win;
+
+function createWindow(){
+    //create browser window
+    win = new BrowserWindow({width:1000, height:400, icon:__dirname+'/img/file'});
+
+    //load index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+
+    //open devtools
+    win.webContents.openDevTools();
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+//run create window function
+app.on('ready', createWindow);
+
+//quit when all windows are clsoed
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin'){
+        app.quit();
+    }
+})
